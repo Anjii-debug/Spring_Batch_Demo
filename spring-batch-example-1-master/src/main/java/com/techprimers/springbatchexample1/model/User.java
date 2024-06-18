9b0ecf49172d0ccfe51504a7be543388484b7cad
Anjali Malviya <malviya_a@xoriant.com>
2024-06-18 15:13:49 +0530

    Add spring batch

---
+++ spring-batch-example-1-master/src/main/java/com/techprimers/springbatchexample1/model/User.java
@@ -0,0 +1,87 @@
+package com.techprimers.springbatchexample1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class User {
+
+    @Id
+    private Integer id;
+    private String name;
+    private int age;
+    private String dept;
+    private Integer salary;
+    private Date time;
+
+    public User(Integer id, String name,int age, String dept, Integer salary, Date time) {
+        this.id = id;
+        this.name = name;
+        this.age=age;
+        this.dept = dept;
+        this.salary = salary;
+        this.time = time;
+    }
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", dept=" + dept + ", salary=" + salary
+				+ ", time=" + time + "]";
+	}
+    
+    
+}
